fb558272ac0d3dffb673c9d871ad0e4582169759
Matthias Kümmel <mkuemmel@informatik.uni-bremen.de>
2015-11-17 10:53:57 +0100

    Erste Übungsaufgaben

---
+++ CPPLernen-u-prof-anwenden/Kapitel1/Aufgabe1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Moegen" << endl << "taeten wir schon wollen," << endl << "aber koennen" << endl << "haben wir uns nicht getraut!" << endl;
+
+    return 0;
+
+}
---
+++ CPPLernen-u-prof-anwenden/Kapitel1/Aufgabe2.cpp
@@ -0,0 +1,26 @@
+/* Fehlerhaftes Programm gegeben & einkommentiert, gefixte Version ausführbar */
+
+/*
+ * #include <stream>
+ * int main
+ * {
+ *    cout << "Wenn dieser Text", 
+ *    cout >> " auf Ihrem Bildschirm erscheint, ";
+ *    cout << " endl;"
+ *    cout << 'können Sie sich auf die Schulter '
+ *         << "klopfen!" << endl.
+ *    return 0;
+ * }
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Wenn dieser Text";
+    cout << " auf Ihrem Bildschirm erscheint, ";
+    cout << "können Sie sich auf die Schulter klopfen!"<< endl;
+
+    return 0;
+}
