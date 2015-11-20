30df959b446ca4f8d3dc063cb390f07d256674c0
Matthias Kümmel <mkuemmel@informatik.uni-bremen.de>
2015-11-20 12:11:37 +0100

    simple learning stuff

---
+++ CPPLernen-u-prof-anwenden/Kapitel2/Aufgabe4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+float a = 123.456;
+float b = 76.543;
+
+int main(){
+    float summe = a + b;
+    float diff = a - b;
+
+    cout << "Summe:         " << summe << endl;
+    cout << "Differenz:     " << diff << endl;
+
+    return 0;
+}
