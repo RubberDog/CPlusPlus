3085aa1e7a8771bca14d179b13c244b9a66d68ce
Matthias Kümmel <mkuemmel@informatik.uni-bremen.de>
2015-12-13 19:10:23 +0100

    more exercises

---
+++ learncpp/Chapter9/9-2.cpp
@@ -0,0 +1,43 @@
+// Overloading operators
+#include <iostream>
+
+class Cents
+{
+    private:
+        int m_nCents;
+
+    public:
+        Cents(int nCents) { m_nCents = nCents; }
+
+        // Overload Cents + Cents
+        friend Cents operator+(const Cents &c1, const Cents &c2);
+
+        // Overload Cents - Cents
+        friend Cents operator-(const Cents &c1, const Cents &c2);
+
+        int GetCents() { return m_nCents; }
+};
+
+// Note: This function is not a member function
+Cents operator+(const Cents &c1, const Cents &c2)
+{
+    // Use the Cents constructor and operator+(int, int)
+    return Cents(c1.m_nCents + c2.m_nCents);
+}
+
+// Note: This function is not a member function
+Cents operator-(const Cents &c1, const Cents &c2)
+{
+    // Use the Cents constructor and operator-(int, int)
+    return Cents(c1.m_nCents - c2.m_nCents);
+}
+
+int main()
+{
+    Cents cCents1(6);
+    Cents cCents2(8);
+    Cents cCentsSum = cCents1 + cCents2;
+    std::cout << "I have " << cCentsSum.GetCents() << " cents." << std::endl;
+
+    return 0;
+}
